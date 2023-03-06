dc3db5b495cc02955a80b97202c971f4cfb664f3
jkrrrr <christopherharland@protonmail.com>
2023-03-06 18:43:58 +0000

    Functional homepage

--- SeProject/SeProject/urls.py
+++ SeProject/SeProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('MapApp.urls')),
     path('discussions/', include('DiscussionApp.urls')),
+    path('', include('MapApp.urls')),
 ]
