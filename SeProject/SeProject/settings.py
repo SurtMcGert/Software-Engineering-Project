8c726e353f1bec4ea59e8705d4bd99fda05d8dab
jkrrrr <christopherharland@protonmail.com>
2023-03-10 08:48:55 +0000

    fix: static files loading

--- SeProject/SeProject/settings.py
+++ SeProject/SeProject/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
 import sys
+from pathlib import Path
+
 import dj_database_url
 import environ
 from django.core.management.utils import get_random_secret_key
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SeProject.urls'
