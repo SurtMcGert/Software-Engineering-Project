76809084f3b7322219d930920c49fe82b83b4da9
jkrrrr <christopherharland@protonmail.com>
2023-03-10 13:54:52 +0000

    feat: animals api test

--- SeProject/MapApp/tests.py
+++ SeProject/MapApp/tests.py
@@ -11,3 +11,21 @@ class HomepageTest(TestCase):
 
         self.assertEquals(response.status_code, 200)
         
+class AnimalsTest(TestCase):
+    def setup(self):
+        return
+
+    def test_animals(self):
+        response = self.client.get('/api/animals')
+
+        self.assertEquals(response.status_code, 200)
+        
+class AnimalsTest(TestCase):
+    def setup(self):
+        return
+
+    def test_animals(self):
+        response = self.client.get('/api/animals')
+
+        self.assertEquals(response.status_code, 200)
+
