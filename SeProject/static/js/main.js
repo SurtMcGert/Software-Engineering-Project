d41552f8e056c9ec87f8c169c5ac95542ff79425
SurtMcGert <harry.ayre@outlook.com>
2023-03-11 15:15:29 +0000

    added UI

--- SeProject/static/js/main.js
+++ SeProject/static/js/main.js
@@ -1,26 +1,13 @@
 $(document).foundation();
 $(function () {
-    setProjectLogoCell();
+    setNavbarPosition();
 });
-//function to resize an element
-function resizeElem(elem, newWidth) {
-    elem.width = newWidth;
-}
-//function to set the hight of the cell for projectLogo
-function setProjectLogoCell() {
-    const cell = document.getElementById("projectLogoCell");
-    const logos = document.getElementsByClassName("projectLogo");
-    if (cell != null && logos != null) {
-        var height;
-        var width;
-        for (var logo of logos) {
-            if (window.getComputedStyle(logo).display != "none") {
-                height = logo.height;
-                width = logo.width + 10;
-            }
-        }
-        cell.setAttribute("style", "height: " + height + "px!important; " + "width: " + width + "px!important;");
-    }
-}
 
-window.addEventListener("resize", setProjectLogoCell);
+window.addEventListener("resize", setNavbarPosition);
+
+//function to set the position of the navbar
+function setNavbarPosition() {
+    navbar = $(".child2");
+    leftPos = $("body,html").width() / 2 - navbar.width() / 2;
+    navbar.css("left", leftPos + "px");
+}
