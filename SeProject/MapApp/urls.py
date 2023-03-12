aad2340fbfa31a07e0f379368f16644f0d7bcba1
RaheemA05932 <raheem.a05932@gmail.com>
2023-03-12 15:15:21 +0000

    few changes

--- SeProject/MapApp/urls.py
+++ SeProject/MapApp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.map, name='map'), #main page goes to map app
-    path('login', views.sighin, name="login"),
+    path('login', views.sighin, name="login"), 
     path('api/animals', views.api_animals, name="api_animals"), #end point for animal api
     path('api/pois', views.api_pois, name="api_pois") #i have no idea
 ]
