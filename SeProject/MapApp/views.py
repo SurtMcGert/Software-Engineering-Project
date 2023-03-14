e4b5ee3c57d75ad92abf77b036c4479b37293237
Nicolas Fedor <fedronn@gmail.com>
2023-03-14 11:09:38 +0000

    refactor: remove signin from mapapp

--- SeProject/MapApp/views.py
+++ SeProject/MapApp/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.conf import settings
 from .models import Poi
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
 
 import requests
 
@@ -15,21 +13,6 @@ def map(request):
     }
 
     return render(request, 'map.html', context)
-
-# Login page
-def sighin(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-        else:
-            messages.error(request, "Username or Password is wrong")
-            return redirect('login')
-    return render(request, "MapApp/login.html")
 
 # Accessed at /api/animals
 # Forwards any query parameters to the Ninja Animals API
