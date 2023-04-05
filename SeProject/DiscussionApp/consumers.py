104696dd96580a6c96748fa77e6081484632f7b1
jkrrrr <christopherharland@protonmail.com>
2023-04-05 15:56:28 +0100

    feat: chatroom functionality

--- SeProject/DiscussionApp/consumers.py
+++ SeProject/DiscussionApp/consumers.py
@@ -15,24 +15,33 @@ class DiscussionConsumer(AsyncJsonWebsocketConsumer):
                 )
         await self.accept()
         
-        await self.channel_layer.group_send(
-                self.discussionGroupName,
-                {
-                    'type': 'tester_message',
-                    'tester': 'Hello, World!',
-                }
-                )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({'tester': tester,}))
-
-
     async def disconnect(self, close_code):
         print("disconnect")
         await self.channel_layer.group_discard(
                 self.discussionGroupName,
                 self.channel_name
                 )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+                self.discussionGroupName, {
+                    'type': 'chat_message',
+                    'message': message,
+                    'username' : username,
+                    }
+                )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            }))
 
     pass
