68467f66f28182ba591d818c61ea6216cd6a8e73
SurtMcGert <harry.ayre@outlook.com>
2023-04-24 14:45:45 +0100

    added names to areas

--- SeProject/DiscussionApp/views.py
+++ SeProject/DiscussionApp/views.py
@@ -3,17 +3,24 @@ from django.contrib.auth.decorators import login_required
 from MapApp.models import Poi
 from .models import ChatMessage
 
-#view to get the discussion history
+# view to get the discussion history
+
+
 @login_required
 def viewDiscussion(request, aid):
     context = {}
+    point = Poi.objects.get(id=aid)
+    print("aid: " + str(aid))
+    print("name: " + point.name)
+    context['name'] = point.name
     context['aid'] = str(aid)
     messages = ChatMessage.objects.filter(chatroom=aid)
     context['messages'] = messages
     return render(request, 'DiscussionApp/discussion.html', context)
 
+
 def sendMessage(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         message = request.POST['message']
         chat_message = ChatMessage(username=request.user, message=message)
         chat_message.save()
