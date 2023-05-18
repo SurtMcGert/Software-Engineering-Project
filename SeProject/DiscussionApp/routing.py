e089d67fea73f369b13272c85e9cb636c52b9766
SurtMcGert <harry.ayre@outlook.com>
2023-05-18 23:39:06 +0100

    using ws because wss doesnt work

--- SeProject/DiscussionApp/routing.py
+++ SeProject/DiscussionApp/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocketURLPatterns = [
-    re_path(r'wss/discussion/(?P<aid>\w+)/$', consumers.DiscussionConsumer.as_asgi(), name="wsDiscussion"),
+    re_path(r'ws/discussion/(?P<aid>\w+)/$', consumers.DiscussionConsumer.as_asgi(), name="wsDiscussion"),
 ]
--- SeProject/SeProject/asgi.py
+++ SeProject/SeProject/asgi.py
@@ -10,17 +10,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import DiscussionApp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeProject.settings')
-
-application = ProtocolTypeRouter({
-  'https': get_asgi_application(),
-  'websocket': AuthMiddlewareStack(DiscussionApp.routing.websocketURLPatterns)
-})
 django.setup()
 
 application = get_asgi_application()
