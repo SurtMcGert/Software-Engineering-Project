378a884dea3ac079169fccb16a14b1c4a3b778d1
SurtMcGert <harry.ayre@outlook.com>
2023-05-19 12:26:12 +0100

    updated asgi.py

--- SeProject/SeProject/asgi.py
+++ SeProject/SeProject/asgi.py
@@ -11,8 +11,9 @@ import os
 
 import django
 from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeProject.settings')
 django.setup()
 
-application = get_asgi_application()
+application = get_default_application()
